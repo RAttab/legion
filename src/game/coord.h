1317bc57dbb2977997defcd2e20ac1d4c75dfd16
Rémi Attab <remi.attab@gmail.com>
2021-01-17 09:35:24 -0500

    Fix compile error

--- src/game/coord.h
+++ src/game/coord.h
@@ -102,7 +102,7 @@ inline bool rect_intersect(const struct rect *lhs, struct rect *rhs)
 {
     return
         (lhs->top.x >= rhs->bot.x && lhs->bot.x < rhs->top.x) ||
-        (lhs->top.y >= rhs->bot.y && lhs->bot.y < rhs->top.y)
+        (lhs->top.y >= rhs->bot.y && lhs->bot.y < rhs->top.y);
 }
 
 
