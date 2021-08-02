ba740e7815035299dd28ad42ba730ab6e5511c8d
Rémi Attab <remi.attab@gmail.com>
2021-08-02 11:31:53 -0400

    Add vecx

--- src/utils/vec.h
+++ src/utils/vec.h
@@ -5,67 +5,10 @@
 
 #pragma once
 
-#include "common.h"
-
-
 // -----------------------------------------------------------------------------
 // vec64
 // -----------------------------------------------------------------------------
 
-struct vec64
-{
-    uint32_t len, cap;
-    uint64_t vals[];
-};
-
-inline void vec64_free(struct vec64 *vec) { free(vec); }
-inline size_t vec64_len(struct vec64 *vec) { return vec ? vec->len : 0; }
-inline size_t vec64_cap(struct vec64 *vec) { return vec ? vec->cap : 0; }
-
-inline struct vec64 *vec64_reserve(size_t size)
-{
-    struct vec64 *vec = calloc(1, sizeof(*vec) + size * sizeof(vec->vals[0]));
-    vec->cap = size;
-    return vec;
-}
-
-inline struct vec64 *vec64_grow(struct vec64 *vec, size_t size)
-{
-    if (!vec) return vec64_reserve(size);
-    if (size < vec->cap) return vec;
-
-    vec = realloc(vec, size * sizeof(vec->vals[0]));
-    vec->cap = size;
-    return vec;
-}
-
-inline struct vec64 *vec64_append(struct vec64 *vec, uint64_t val)
-{
-    if (unlikely(!vec)) vec = vec64_reserve(1);
-    if (unlikely(vec->len == vec->cap)) {
-        size_t cap = vec->cap * 2;
-        vec = realloc(vec, sizeof(*vec) + cap * sizeof(vec->vals[0]));
-        vec->cap = cap;
-    }
-    vec->vals[vec->len++] = val;
-    return vec;
-}
-
-inline struct vec64 *vec64_sort(struct vec64 *vec)
-{
-    // gcc extension
-    int cmp(const void *l, const void *r) {
-        uint64_t lhs = *((uint64_t *) l);
-        uint64_t rhs = *((uint64_t *) r);
-        return lhs == rhs ? 0 : (lhs < rhs ? -1 : 1);
-    }
-    qsort(vec->vals, vec->len, sizeof(vec->vals[0]), cmp);
-}
-
-inline bool vec64_eq(const struct vec64 *lhs, const struct vec64 *rhs)
-{
-    if (lhs->len != rhs->len) return false;
-    for (size_t i = 0; i < lhs->len; ++i)
-        if (lhs->vals[i] != rhs->vals[i]) return false;
-    return true;
-}
+#define vecx_type uint64_t
+#define vecx_name vec64
+#include "utils/vecx.h"
---
+++ src/utils/vecx.h
@@ -0,0 +1,96 @@
+/* vecx.h
+   Rémi Attab (remi.attab@gmail.com), 02 Aug 2021
+   FreeBSD-style copyright and disclaimer apply
+*/
+
+#ifndef vecx_type
+# error "vecx_type must be declared when including vecx.h"
+#endif
+
+#ifndef vecx_name
+# error "vecx_name must be declared when including vecx.h"
+#endif
+
+#define vecx_fn_concat(prefix, suffix) prefix ## _ ## suffix
+#define vecx_fn_eval(prefix, suffix) vecx_fn_concat(prefix, suffix)
+#define vecx_fn(name) vecx_fn_eval(vecx_name, name)
+
+
+#include "common.h"
+
+
+// -----------------------------------------------------------------------------
+// vecx
+// -----------------------------------------------------------------------------
+
+struct vecx_name
+{
+    uint32_t len, cap;
+    vecx_type vals[];
+};
+
+inline void vecx_fn(free) (struct vecx_name *vec) { free(vec); }
+inline size_t vecx_fn(len) (struct vecx_name *vec) { return vec ? vec->len : 0; }
+inline size_t vecx_fn(cap) (struct vecx_name *vec) { return vec ? vec->cap : 0; }
+
+inline struct vecx_name *vecx_fn(reserve) (size_t size)
+{
+    struct vecx_name *vec = calloc(1, sizeof(*vec) + size * sizeof(vec->vals[0]));
+    vec->cap = size;
+    return vec;
+}
+
+inline struct vecx_name *vecx_fn(grow) (struct vecx_name *vec, size_t size)
+{
+    if (!vec) return vecx_fn(reserve)(size);
+    if (size < vec->cap) return vec;
+
+    vec = realloc(vec, size * sizeof(vec->vals[0]));
+    vec->cap = size;
+    return vec;
+}
+
+inline struct vecx_name *vecx_fn(append) (struct vecx_name *vec, uint64_t val)
+{
+    if (unlikely(!vec)) vec = vecx_fn(reserve)(1);
+    if (unlikely(vec->len == vec->cap)) {
+        size_t cap = vec->cap * 2;
+        vec = realloc(vec, sizeof(*vec) + cap * sizeof(vec->vals[0]));
+        vec->cap = cap;
+    }
+    vec->vals[vec->len++] = val;
+    return vec;
+}
+
+inline struct vecx_name *vecx_fn(copy) (struct vecx_name *old)
+{
+    struct vecx_name *new = vecx_fn(grow) (NULL, old->cap);
+    memcpy(new, old, sizeof(*old) + old->len * sizeof(old->val[0]));
+    return new;
+}
+
+inline struct vecx_name *vecx_fn(sort) (struct vecx_name *vec)
+{
+    // gcc extension
+    int cmp(const void *l, const void *r) {
+        uint64_t lhs = *((uint64_t *) l);
+        uint64_t rhs = *((uint64_t *) r);
+        return lhs == rhs ? 0 : (lhs < rhs ? -1 : 1);
+    }
+    qsort(vec->vals, vec->len, sizeof(vec->vals[0]), cmp);
+}
+
+inline bool vecx_fn(eq) (const struct vecx_name *lhs, const struct vecx_name *rhs)
+{
+    if (lhs->len != rhs->len) return false;
+    for (size_t i = 0; i < lhs->len; ++i)
+        if (lhs->vals[i] != rhs->vals[i]) return false;
+    return true;
+}
+
+#undef vecx_fn_concat
+#undef vecx_fn_eval
+#undef vecx_fn
+
+#undef vecx_type
+#undef vecx_name
