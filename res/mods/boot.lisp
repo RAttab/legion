01ef96d2c0033a36bc9e1a5cc29ad15714f71133
Rémi Attab <remi.attab@gmail.com>
2021-08-23 12:46:33 -0400

    Cleanup function names in lisp mods

--- res/mods/boot.lisp
+++ res/mods/boot.lisp
@@ -1,24 +1,23 @@
 ;; Bootstraps a basic factory along a few instances of `&item_legion`
 ;; that are sent to other stars in the sector via `(mod launch 2)`
 
+(build-extract 2)
+(build-printer 2)
+(build-extract 4)
+(build-printer 4)
+(build-assembly 2)
 
-(boot/extract 2)
-(boot/printer 2)
-(boot/extract 4)
-(boot/printer 4)
-(boot/assembler 2)
-
-(boot/launch)
-(boot/legion 3)
+(launch-legion)
+(build-legion 3)
 
 
 ;; -----------------------------------------------------------------------------
 ;; extract
 ;; -----------------------------------------------------------------------------
 
-(defun boot/extract (n)
+(defun build-extract (n)
   (let ((items 6)
-	(current (boot/count &item_extract)))
+	(current (count &item_extract)))
     (when (<= current 2)
       (io &io_tape (id &item_extract 1) &item_elem_a)
       (io &io_tape (id &item_extract 2) &item_elem_b)
@@ -27,17 +26,17 @@
 
     (io &io_tape (id &item_assembly 1) &item_extract (- (* items n) current))
     (io &io_item (id &item_deploy 1) &item_extract (- (* items n) current))
-    (boot/wait (id &item_deploy 1)))
+    (wait (id &item_deploy 1)))
 
   (let ((id 1))
-    (set id (boot/extract-set id n &item_elem_a))
-    (set id (boot/extract-set id n &item_elem_b))
-    (set id (boot/extract-set id n &item_elem_c))
-    (set id (boot/extract-set id n &item_elem_d))
-    (set id (boot/extract-set id n &item_elem_e))
-    (set id (boot/extract-set id n &item_elem_f))))
+    (set id (set-extract id n &item_elem_a))
+    (set id (set-extract id n &item_elem_b))
+    (set id (set-extract id n &item_elem_c))
+    (set id (set-extract id n &item_elem_d))
+    (set id (set-extract id n &item_elem_e))
+    (set id (set-extract id n &item_elem_f))))
 
-(defun boot/extract-set (id n item)
+(defun set-extract (id n item)
   (for (i 0) (< i n) (+ i 1) (io &io_tape (id &item_extract (+ id i)) item))
   (+ id n))
 
@@ -46,27 +45,27 @@
 ;; printer
 ;; -----------------------------------------------------------------------------
 
-(defun boot/printer (n)
+(defun build-printer (n)
   (let ((items 7)
-	(current (boot/count &item_printer)))
+	(current (count &item_printer)))
     (when (<= current 2)
       (io &io_tape (id &item_printer 1) &item_logic)
       (io &io_tape (id &item_printer 2) &item_gear))
 
     (io &io_tape (id &item_assembly 1) &item_printer (- (* items n) current))
     (io &io_item (id &item_deploy 1) &item_printer (- (* items n) current))
-    (boot/wait (id &item_deploy 1)))
+    (wait (id &item_deploy 1)))
 
   (let ((id 1))
-    (set id (boot/printer-set id n &item_frame))
-    (set id (boot/printer-set id n &item_logic))
-    (set id (boot/printer-set id n &item_gear))
-    (set id (boot/printer-set id n &item_neuron))
-    (set id (boot/printer-set id n &item_bond))
-    (set id (boot/printer-set id n &item_magnet))
-    (set id (boot/printer-set id n &item_nuclear))))
+    (set id (set-printer id n &item_frame))
+    (set id (set-printer id n &item_logic))
+    (set id (set-printer id n &item_gear))
+    (set id (set-printer id n &item_neuron))
+    (set id (set-printer id n &item_bond))
+    (set id (set-printer id n &item_magnet))
+    (set id (set-printer id n &item_nuclear))))
 
-(defun boot/printer-set (id n item)
+(defun set-printer (id n item)
   (for (i 0) (< i n) (+ i 1) (io &io_tape (id &item_printer (+ id i)) item))
   (+ id n))
 
@@ -75,32 +74,32 @@
 ;; assembler
 ;; -----------------------------------------------------------------------------
 
-(defun boot/assembler (n)
+(defun build-assembly (n)
   (let ((items 7)
-	(current-asm (- (boot/count &item_assembly) 2)) ; we always reserve 2 assemblies for our internal needs
-	(current-work (boot/count &item_worker)))
+	(current-asm (- (count &item_assembly) 2)) ; we always reserve 2 assemblies for our internal needs
+	(current-work (count &item_worker)))
 
     (when (< current-asm (* n items))
       (io &io_tape (id &item_assembly 2) &item_core)
       (io &io_tape (id &item_assembly 1) &item_worker (- (* n 10) current-work))
       (io &io_item (id &item_deploy 1) &item_worker (- (* n 10) current-work))
-      (boot/wait (id &item_deploy 1))
+      (wait (id &item_deploy 1))
 
       (io &io_tape (id &item_assembly 2) &item_robotics)
       (io &io_tape (id &item_assembly 1) &item_assembly (- (* n items) current-asm))
       (io &io_item (id &item_deploy 1) &item_assembly (- (* n items) current-asm))
-      (boot/wait (id &item_deploy 1))))
+      (wait (id &item_deploy 1))))
 
   (let ((id 3))
-    (set id (boot/assembly-set id n &item_robotics))
-    (set id (boot/assembly-set id n &item_core))
-    (set id (boot/assembly-set id n &item_capacitor))
-    (set id (boot/assembly-set id n &item_matrix))
-    (set id (boot/assembly-set id n &item_magnet_field))
-    (set id (boot/assembly-set id n &item_hull))
-    (set id (boot/assembly-set id n &item_memory))))
+    (set id (set-assembly id n &item_robotics))
+    (set id (set-assembly id n &item_core))
+    (set id (set-assembly id n &item_capacitor))
+    (set id (set-assembly id n &item_matrix))
+    (set id (set-assembly id n &item_magnet_field))
+    (set id (set-assembly id n &item_hull))
+    (set id (set-assembly id n &item_memory))))
 
-(defun boot/assembly-set (id n item)
+(defun set-assembly (id n item)
   (for (i 0) (< i n) (+ i 1) (io &io_tape (id &item_assembly (+ id i)) item))
   (+ id n))
 
@@ -109,14 +108,14 @@
 ;; launch
 ;; -----------------------------------------------------------------------------
 
-(defun boot/launch ()
+(defun launch-legion ()
   (io &io_tape (id &item_assembly 1) &item_scanner 2)
   (io &io_item (id &item_deploy 1) &item_scanner 2)
-  (boot/wait (id &item_deploy 1))
+  (wait (id &item_deploy 1))
 
   (io &io_tape (id &item_assembly 1) &item_brain 1)
   (io &io_item (id &item_deploy 1) &item_brain 1)
-  (boot/wait (id &item_deploy 1))
+  (wait (id &item_deploy 1))
 
   (assert (= (io &io_mod (id &item_brain 2) (mod launch 2)) &io_ok)))
 
@@ -125,41 +124,41 @@
 ;; legion
 ;; -----------------------------------------------------------------------------
 
-(defun boot/legion (n)
+(defun build-legion (n)
   (let ((items 7)
-	(id (+ (boot/count &item_assembly) 1)))
+	(id (+ (count &item_assembly) 1)))
 
     (io &io_tape (id &item_assembly 1) &item_assembly items)
     (io &io_item (id &item_deploy 1) &item_assembly items)
-    (boot/wait (id &item_deploy 1))
+    (wait (id &item_deploy 1))
 
-    (set id (boot/assembly-set id 1 &item_deploy))
-    (set id (boot/assembly-set id 1 &item_extract))
-    (set id (boot/assembly-set id 1 &item_printer))
-    (set id (boot/assembly-set id 1 &item_assembly))
-    (set id (boot/assembly-set id 1 &item_scanner))
-    (set id (boot/assembly-set id 1 &item_brain))
-    (set id (boot/assembly-set id 1 &item_worker)))
+    (set id (set-assembly id 1 &item_deploy))
+    (set id (set-assembly id 1 &item_extract))
+    (set id (set-assembly id 1 &item_printer))
+    (set id (set-assembly id 1 &item_assembly))
+    (set id (set-assembly id 1 &item_scanner))
+    (set id (set-assembly id 1 &item_brain))
+    (set id (set-assembly id 1 &item_worker)))
 
   (io &io_tape (id &item_assembly 1) &item_legion n)
   (io &io_item (id &item_deploy 1) &item_legion n)
-  (boot/wait (id &item_deploy 1)))
+  (wait (id &item_deploy 1)))
 
 
 ;; -----------------------------------------------------------------------------
 ;; utils
 ;; -----------------------------------------------------------------------------
 
-(defun boot/wait (id)
+(defun wait (id)
   (while (if (= (io &io_status id) &io_ok) (/= (head) 0))
     (yield)))
 
-(defun boot/coord ()
+(defun coord ()
   (assert (= (io &io_coord 0) &io_ok))
   (head))
 
-(defun boot/count (item)
-  (let ((coord (boot/coord)))
+(defun count (item)
+  (let ((coord (coord)))
     (io &io_scan (id &item_scanner 1) coord item)
     (io &io_scan_val (id &item_scanner 1))
     (head)))
