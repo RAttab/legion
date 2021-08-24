ce9fed34916201304a30b4a3ffc4feb6985de650
Rémi Attab <remi.attab@gmail.com>
2021-08-24 12:01:02 -0400

    Add profiling to compilation

--- compile.sh
+++ compile.sh
@@ -2,9 +2,10 @@
 
 set -o errexit -o nounset -o pipefail # -o xtrace
 
+CC=${CC:-gcc}
 : ${PREFIX:="."}
-: ${VM_DEBUG:=""}
 : ${VALGRIND:=""}
+: ${PROFILE:=""}
 
 declare -a SRC
 SRC=(items ui render game vm utils)
@@ -12,7 +13,10 @@ SRC=(items ui render game vm utils)
 declare -a TEST
 : ${TEST:="coord ring text lisp chunk lanes save world"}
 
-CC=${CC:-gcc}
+TIMEFORMAT="%3R"
+TIME="eval time"
+ECHO="eval echo -n"
+if [ -z "${PROFILE}" ]; then TIME=""; ECHO=""; fi
 
 CFLAGS="-ggdb -O3 -march=native -pipe -std=gnu11 -D_GNU_SOURCE -lm"
 CFLAGS="$CFLAGS -I${PREFIX}/src"
@@ -30,32 +34,35 @@ CFLAGS="$CFLAGS -Wformat=2"
 CFLAGS="$CFLAGS -Winit-self"
 CFLAGS="$CFLAGS -Wno-implicit-fallthrough"
 CFLAGS="$CFLAGS -Wno-address-of-packed-member" # very annoying
-if [ ! -z "${VM_DEBUG}" ]; then CFLAGS="$CFLAGS -DVM_DEBUG"; fi
 
 LIBS="liblegion.a"
 LIBS="$LIBS $(sdl2-config --libs)"
 LIBS="$LIBS $(pkg-config --libs freetype2)"
 
 mkdir -p "obj"
-parallel $CC -c -o "obj/{}.o" "${PREFIX}/src/{}.c" $CFLAGS ::: ${SRC[@]}
+$ECHO "object compilation..."
+$TIME parallel $CC -c -o "obj/{}.o" "${PREFIX}/src/{}.c" $CFLAGS ::: ${SRC[@]}
 
 OBJ=""
 for obj in "${SRC[@]}"; do OBJ="$OBJ obj/${obj}.o"; done
 ar rcs liblegion.a $OBJ
 
-$CC -o "legion" "${PREFIX}/src/main.c" $LIBS $CFLAGS
+$ECHO "exec compilation..."
+$TIME $CC -o "legion" "${PREFIX}/src/main.c" $LIBS $CFLAGS
 cp -r "${PREFIX}/res" .
 
 ./legion --viz | dot -Tsvg > tapes.svg
 ./legion --stats > stats.lisp
 
 mkdir -p "test"
-parallel $CC -o "test/{}" "${PREFIX}/test/{}_test.c" $LIBS $CFLAGS ::: ${TEST[@]}
+$ECHO "test compilation..."
+$TIME parallel $CC -o "test/{}" "${PREFIX}/test/{}_test.c" $LIBS $CFLAGS ::: ${TEST[@]}
 
+$ECHO "test execution..."
 if [ -z "${VALGRIND}" ]; then
-    parallel "./test/{}" "${PREFIX}" ::: ${TEST[@]}
+    $TIME parallel "./test/{}" "${PREFIX}" ::: ${TEST[@]}
 else
-    parallel valgrind \
+    $TIME parallel valgrind \
         --leak-check=full \
         --track-origins=yes \
         --trace-children=yes \
