8952d9c063aa965a28677d2ee3054953afe3daae
Rémi Attab <remi.attab@gmail.com>
2021-08-30 14:38:20 -0400

    Use defconst in boot.lisp

--- res/mods/boot.lisp
+++ res/mods/boot.lisp
@@ -7,34 +7,47 @@
 ;; (mod launch 2) module and research to get to antenna is done via
 ;; the (mod lab 2) module.
 
+(defconst extract-count 2)
+(defconst condenser-count 4)
+(defconst printer-count 4)
+(defconst assembly-count 2)
+(defconst worker-count 20)
+(defconst lab-count 4)
+(defconst active-count 1)
+(defconst legion-count 2)
+(defconst antenna-count (+ legion-count 1))
+
+(defconst energy-target 1000)
+(defconst specs-solar-div 100)
+
 
 ;; Elem - Extract
-(let ((n 2))
+(progn
   (set-tape 1 1 &item_extract &item_elem_a)
   (set-tape 2 1 &item_extract &item_elem_b)
   (set-tape 1 1 &item_printer &item_frame)
   (set-tape 2 1 &item_printer &item_logic)
 
-  (deploy-tape &item_extract &item_elem_b n)
+  (deploy-tape &item_extract &item_elem_b extract-count)
   (set-tape 1 2 &item_extract &item_elem_a)
-  (deploy-tape &item_extract &item_elem_c n)
-  (deploy-tape &item_extract &item_elem_d n)
-  (deploy-tape &item_extract &item_elem_e n)
-  (deploy-tape &item_extract &item_elem_f n))
+  (deploy-tape &item_extract &item_elem_c extract-count)
+  (deploy-tape &item_extract &item_elem_d extract-count)
+  (deploy-tape &item_extract &item_elem_e extract-count)
+  (deploy-tape &item_extract &item_elem_f extract-count))
 
 
 ;; Printers - T0
-(let ((n 4))
+(progn
   (set-tape 1 1 &item_printer &item_logic)
   (set-tape 2 1 &item_printer &item_gear)
 
-  (deploy-tape &item_printer &item_frame (- n 2))
-  (deploy-tape &item_printer &item_logic n)
-  (deploy-tape &item_printer &item_gear n)
-  (deploy-tape &item_printer &item_neuron n)
-  (deploy-tape &item_printer &item_bond n)
-  (deploy-tape &item_printer &item_magnet n)
-  (deploy-tape &item_printer &item_nuclear n)
+  (deploy-tape &item_printer &item_frame (- printer-count 2))
+  (deploy-tape &item_printer &item_logic printer-count)
+  (deploy-tape &item_printer &item_gear printer-count)
+  (deploy-tape &item_printer &item_neuron printer-count)
+  (deploy-tape &item_printer &item_bond printer-count)
+  (deploy-tape &item_printer &item_magnet printer-count)
+  (deploy-tape &item_printer &item_nuclear printer-count)
 
   (set-tape 1 2 &item_printer &item_frame))
 
@@ -42,109 +55,108 @@
 ;; Workers
 (progn
   (set-tape 1 2 &item_assembly &item_core)
-  (deploy-item &item_worker 20))
+  (deploy-item &item_worker worker-count))
 
 
 ;; Assembly - T0 Passive
-(let ((n 2))
+(progn
   (set-tape 2 1 &item_assembly &item_robotics)
 
-  (deploy-tape &item_assembly &item_robotics n)
-  (deploy-tape &item_assembly &item_core n)
-  (deploy-tape &item_assembly &item_capacitor n)
-  (deploy-tape &item_assembly &item_matrix n)
-  (deploy-tape &item_assembly &item_magnet_field n)
-  (deploy-tape &item_assembly &item_hull n)
+  (deploy-tape &item_assembly &item_robotics assembly-count)
+  (deploy-tape &item_assembly &item_core assembly-count)
+  (deploy-tape &item_assembly &item_capacitor assembly-count)
+  (deploy-tape &item_assembly &item_matrix assembly-count)
+  (deploy-tape &item_assembly &item_magnet_field assembly-count)
+  (deploy-tape &item_assembly &item_hull assembly-count)
   ;; Not passive but requirements in other recipies
-  (deploy-tape &item_assembly &item_memory n)
-  (deploy-tape &item_assembly &item_worker n)
+  (deploy-tape &item_assembly &item_memory assembly-count)
+  (deploy-tape &item_assembly &item_worker assembly-count)
 
   (assert (= (io &io_reset (id &item_assembly 2)) &io_ok)))
 
 
 ;; Research
-(let ((n 4))
-  (deploy-item &item_lab n)
+(progn
+  (deploy-item &item_lab lab-count)
   (deploy-item &item_brain 1)
 
   (let ((id-brain (id &item_brain (count &item_brain))))
     (assert (= (io &io_mod id-brain (mod lab 2)) &io_ok))
-    (assert (= (io &io_send id-brain !lab_count n) &io_ok))))
+    (assert (= (io &io_send id-brain !lab_count lab-count) &io_ok))))
 
 
 ;; Legion
-(let ((n 2))
+(progn
   (deploy-item &item_scanner 2)
   (deploy-item &item_brain 1)
   (let ((id-brain (id &item_brain (count &item_brain))))
     (assert (= (io &io_mod id-brain (mod launch 2)) &io_ok)))
 
-  (deploy-tape &item_assembly &item_deploy 1)
-  (deploy-tape &item_assembly &item_extract 1)
-  (deploy-tape &item_assembly &item_printer 1)
-  (deploy-tape &item_assembly &item_assembly 1)
-  (deploy-tape &item_assembly &item_scanner 1)
-  (deploy-tape &item_assembly &item_brain 1)
-  (deploy-item &item_legion n))
+  (deploy-tape &item_assembly &item_deploy active-count)
+  (deploy-tape &item_assembly &item_extract active-count)
+  (deploy-tape &item_assembly &item_printer active-count)
+  (deploy-tape &item_assembly &item_assembly active-count)
+  (deploy-tape &item_assembly &item_scanner active-count)
+  (deploy-tape &item_assembly &item_brain active-count)
+  (deploy-item &item_legion legion-count))
 
 
 ;; Energy - Solar
-(let ((target 1000))
+(progn
   (wait-tech &item_captor)
-  (deploy-tape &item_printer &item_captor 4)
+  (deploy-tape &item_printer &item_captor printer-count)
 
   (wait-tech &item_solar)
-  (deploy-tape &item_assembly &item_solar 2)
-  (deploy-item &item_solar (/ target (/ (count &item_energy) 100))))
+  (deploy-tape &item_assembly &item_solar assembly-count)
+  (deploy-item &item_solar (/ energy-target (/ (count &item_energy) specs-solar-div))))
 
 
 ;; Elem - Condenser
-(let ((n 4))
-
+(progn
   (wait-tech &item_condenser)
-  (deploy-tape &item_condenser &item_elem_g n)
-  (deploy-tape &item_condenser &item_elem_h n)
-  (deploy-tape &item_condenser &item_elem_i n)
-  (deploy-tape &item_condenser &item_elem_j n))
+  (deploy-tape &item_condenser &item_elem_g condenser-count)
+  (deploy-tape &item_condenser &item_elem_h condenser-count)
+  (deploy-tape &item_condenser &item_elem_i condenser-count)
+  (deploy-tape &item_condenser &item_elem_j condenser-count))
 
 
 ;; Antenna
-(let ((n 2)) ;; must match legion's count
+(progn
   (wait-tech &item_liquid_frame)
-  (deploy-tape &item_printer &item_liquid_frame 4)
+  (deploy-tape &item_printer &item_liquid_frame printer-count)
 
   (wait-tech &item_radiation)
-  (deploy-tape &item_printer &item_radiation 4)
+  (deploy-tape &item_printer &item_radiation printer-count)
 
   (wait-tech &item_antenna)
-  (deploy-tape &item_assembly &item_antenna 2)
+  (deploy-tape &item_assembly &item_antenna assembly-count)
 
   (wait-tech &item_transmit)
-  (deploy-item &item_transmit (+ n 1))
+  (deploy-item &item_transmit antenna-count)
 
   (wait-tech &item_receive)
-  (deploy-item &item_receive (+ n 1)))
+  (deploy-item &item_receive antenna-count))
 
 
 ;; Finish - T1
 (progn
   (wait-tech &item_storage)
-  (deploy-tape &item_assembly &item_storage 1)
+  (deploy-tape &item_assembly &item_storage active-count)
 
   (wait-tech &item_accelerator)
-  (deploy-tape &item_assembly &item_accelerator 2)
+  (deploy-tape &item_assembly &item_accelerator assembly-count)
 
-  (deploy-tape &item_assembly &item_lab 1))
+  (deploy-tape &item_assembly &item_lab active-count))
 
 
 ;; Spanning Tree
-(let ((n 2)) ;; must match antenna's count
+(progn
   (while (count &item_legion))
 
   ;; Launch is responsible for filling (id &item_memory 1) with our
   ;; children stars while the parent is filled in automatically when
   ;; legion is deployed.
-  (for (i 0) (< i (+ n 1)) (+ i 1)
+  (for (i 0) (< i antenna-count) (+ i 1)
        (let ((coord (progn (io &io_get (id &item_memory 1) i) (head))))
 	 (assert (= (io &io_target (id &item_transmit (+ i 1)) coord) &io_ok))
 	 (assert (= (io &io_target (id &item_receive (+ i 1)) coord) &io_ok)))))
