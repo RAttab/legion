8beba90984f02ccc8c0518d61a28d9e058601ce8
Rémi Attab <remi.attab@gmail.com>
2021-10-18 12:53:57 -0400

    Update t1 tape for new tech tree

--- res/tapes/db.t1.lisp
+++ res/tapes/db.t1.lisp
@@ -1,7 +1,9 @@
 ;; -----------------------------------------------------------------------------
 ;; condenser
 ;; -----------------------------------------------------------------------------
+
 (item_condenser
+
  (item_elem_g (energy 1) (out item_elem_g))
  (item_elem_h (energy 1) (out item_elem_h)))
 
@@ -12,130 +14,191 @@
 
 (item_printer
 
- (item_captor
-  (in item_elem_c
-      item_elem_e
-      item_elem_c
-      item_elem_c
-      item_elem_c
-      item_elem_e
-      item_elem_c)
-  (out item_captor))
+ (item_magnet
+  (in item_elem_d
+      item_elem_d
+      item_elem_b
+      item_elem_b
+      item_elem_d
+      item_elem_d)
+  (out item_magnet))
 
- (item_liquid_frame
-  (energy 4)
+ (item_ferrofluid
+  (in item_elem_d
+      item_elem_c
+      item_elem_d
+      item_elem_c
+      item_elem_d)
+  (out item_ferrofluid))
+
+ (item_semiconductor
+  (in item_elem_d
+      item_elem_d
+      item_elem_a
+      item_elem_c
+      item_elem_c
+      item_elem_a
+      item_elem_d
+      item_elem_d)
+  (out item_semiconductor))
+
+ (item_conductor
   (in item_elem_g
       item_elem_g
-      item_elem_a
       item_elem_g
+      item_elem_d
+      item_elem_d
       item_elem_g
-      item_elem_a
       item_elem_g
       item_elem_g)
-  (out item_liquid_frame))
+  (out item_conductor))
 
- (item_radiation
-  (energy 10)
-  (in item_elem_f
-      item_elem_h
-      item_elem_f
+ (item_galvanic
+  (in item_elem_g
       item_elem_h
       item_elem_h
+      item_elem_g
+      item_elem_g
       item_elem_h
-      item_elem_f
       item_elem_h
-      item_elem_f)
-  (out item_radiation)))
+      item_elem_g)
+  (out item_galvanic)))
+
 
 ;; -----------------------------------------------------------------------------
-;; assembly
+;; assembly - passive
+;; -----------------------------------------------------------------------------
+
+(item_assembly
+
+ (item_photovoltaic
+  (in item_semiconductor
+      item_semiconductor
+      item_nerve
+      item_bone
+      item_bone
+      item_nerve
+      item_semiconductor
+      item_semiconductor)
+  (out item_photovoltaic))
+
+ (item_field
+  (in item_magnet
+      item_magnet
+      item_magnet
+      item_nerve
+      item_nerve
+      item_magnet
+      item_magnet
+      item_magnet)
+  (out item_field))
+
+ (item_antenna
+  (in item_bone
+      item_bone
+      item_nerve
+      item_conductor
+      item_conductor
+      item_nerve
+      item_conductor
+      item_conductor
+      item_nerve
+      item_bone
+      item_bone)
+  (out item_antenna))
+
+ (item_accelerator
+  (in item_conductor
+      item_conductor
+      item_nerve
+      item_field
+      item_field
+      item_nerve
+      item_conductor
+      item_conductor)
+  (out item_accelerator)))
+
+
+;; -----------------------------------------------------------------------------
+;; assembly - active
 ;; -----------------------------------------------------------------------------
 
 (item_assembly
 
  (item_solar
-  (in item_captor
-      item_captor
-      item_bond
-      item_captor
-      item_bond
-      item_captor
-      item_bond
-      item_captor
-      item_captor)
+  (in item_photovoltaic
+      item_nerve
+      item_nodule
+      item_nerve
+      item_photovoltaic)
   (out item_solar))
 
  (item_storage
-  (in item_magnet_field
-      item_robotics
-      item_robotics
-      item_robotics
-      item_magnet_field)
+  (in item_bone
+      item_bone
+      item_vein
+      item_vein
+      item_vein
+      item_bone
+      item_bone)
   (out item_storage))
 
  (item_port
-  (in item_magnet_field
-      item_magnet_field
-      item_captor
-      item_frame
-      item_storage
-      item_storage
-      item_frame
-      item_captor
-      item_magnet_field
-      item_magnet_field)
+  (in item_field
+      item_field
+      item_field
+      item_field
+      item_field
+      item_nodule
+      item_limb
+      item_limb
+      item_limb
+      item_nodule
+      item_storage)
   (out item_port))
 
  (item_pill
-  (in item_hull
-      item_magnet
-      item_magnet
-      item_hull)
+  (in item_vein
+      item_ferrofluid
+      item_vein
+      item_bone
+      item_bone
+      item_vein
+      item_ferrofluid
+      item_vein)
   (out item_pill))
 
  (item_condenser
-  (in item_worker
-      item_nuclear
-      item_nuclear
-      item_nuclear
-      item_worker)
+  (in item_lung
+      item_lung
+      item_nodule
+      item_lung
+      item_lung
+      item_lung)
   (out item_condenser))
 
- (item_energy_store
+ (item_battery
   (energy 1)
-  (in item_captor
-      item_liquid_frame
-      item_capacitor
-      item_liquid_frame
-      item_captor)
-  (out item_energy_store))
+  (in item_bone
+      item_nerve
+      item_galvanic
+      item_galvanic
+      item_galvanic
+      item_nerve
+      item_bone)
+  (out item_battery))
 
  ;; (item_auto_deploy
  ;;  (energy 1)
- ;;  (in item_liquid_frame
- ;;      item_core
- ;;      item_deploy
- ;;      item_core
- ;;      item_liquid_frame)
+ ;;  (in )
  ;;  (out item_auto_deploy))
-
- (item_antenna
-  (energy 10)
-  (in item_liquid_frame
-      item_radiation
-      item_radiation
-      item_radiation
-      item_liquid_frame
-      item_liquid_frame
-      item_liquid_frame)
-  (out item_antenna))
 
  (item_transmit
   (energy 20)
-  (in item_memory
-      item_memory
-      item_antenna
-      item_antenna
+  (in item_eye
+      item_eye
+      item_eye
+      item_nodule
       item_antenna
       item_antenna
       item_antenna)
@@ -146,29 +209,20 @@
   (in item_antenna
       item_antenna
       item_antenna
-      item_antenna
-      item_antenna
-      item_memory
-      item_memory)
+      item_nodule
+      item_engram
+      item_engram
+      item_engram)
   (out item_receive))
 
- (item_accelerator
-  (energy 20)
-  (in item_liquid_frame
-      item_magnet_field
-      item_magnet_field
-      item_magnet_field
-      item_liquid_frame)
-  (out item_accelerator))
-
  (item_collider
-  (energy 30)
-  (in item_lab
+  (energy 100)
+  (in item_brain
+      item_nodule
       item_accelerator
       item_accelerator
+      item_nodule
       item_accelerator
       item_accelerator
-      item_accelerator
-      item_lab)
-  (out item_collider))
- )
+      item_nodule)
+  (out item_collider)))
